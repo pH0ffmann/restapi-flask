1f6e70617a9fa54fb3d08ba676692e17cc234e3c
root <root@DESKTOP-OHN0TFT.>
2024-12-02 20:12:08 -0300

    feat: reqparse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
@@ -13,6 +13,34 @@ app.config["MONGODB_SETTINGS"] = [
         "password": "admin"
     }
 ]
+
+_user_parser = reqparse.RequestParser()
+
+_user_parser.add_argument('first_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('last_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('cpf',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('birth_date',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
 
 api = Api(app)
 db = MongoEngine(app)
@@ -33,7 +61,8 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"message": "teste"}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
 
     def get(self, cpf):
         return {"message": "CPF"}
