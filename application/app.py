ea810bd7f1528f8575874db1aef9c272de310629
root <root@DESKTOP-OHN0TFT.>
2024-12-03 01:40:43 -0300

    refactor: using application factory

--- application/app.py
+++ application/app.py
@@ -1,20 +1,8 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-from flask_mongoengine import MongoEngine
+from flask import jsonify
+from flask_restful import Resource, reqparse
 from mongoengine import NotUniqueError
+from .model import UserModel
 import re
-
-app = Flask(__name__)
-
-app.config["MONGODB_SETTINGS"] = [
-    {
-        "db": "users",
-        "host": "mongodb",
-        "port": 27017,
-        "username": "admin",
-        "password": "admin"
-    }
-]
 
 _user_parser = reqparse.RequestParser()
 
@@ -44,74 +32,49 @@ _user_parser.add_argument('birth_date',
                           help="This field cannot be blank."
                           )
 
-api = Api(app)
-db = MongoEngine(app)
-
-
-class UserModel(db.Document):
-    cpf = db.StringField(required=True, unique=True)
-    first_name = db.StringField(required=True)
-    last_name = db.StringField(required=True)
-    email = db.EmailField(required=True)
-    birth_date = db.DateTimeField(required=True)
-
 
 class Users(Resource):
     def get(self):
-        return {"message": "user 1"}
+        return jsonify(UserModel.objects())
 
 
 class User(Resource):
     def validate_cpf(self, cpf):
-
         # Has the correct mask?
         if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
             return False
-
         # Grab only numbers
         numbers = [int(digit) for digit in cpf if digit.isdigit()]
-
         # Does it have 11 digits?
         if len(numbers) != 11 or len(set(numbers)) == 1:
             return False
-
         # Validate first digit after -
         sum_of_products = sum(a*b for a, b in zip(numbers[0:9],
                                                   range(10, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[9] != expected_digit:
             return False
-
         # Validate second digit after -
         sum_of_products = sum(a*b for a, b in zip(numbers[0:10],
                                                   range(11, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[10] != expected_digit:
             return False
-
         return True
-
 
     def post(self):
         data = _user_parser.parse_args()
-        
         if not self.validate_cpf(data["cpf"]):
             return {"message": "CPF is invalid"}, 400
-        
         try:
             response = UserModel(**data).save()
             return {"message": "User %s sucessfully created!" % response.id}
         except NotUniqueError:
             return {"message": "CPF already exists in database!"}, 400
 
-
-
     def get(self, cpf):
-        return {"message": "CPF"}
+        response = UserModel.objects(cpf=cpf)
 
-
-api.add_resource(Users, "/users")
-api.add_resource(User, "/user", "/user/<string:cpf>")
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+        if response:
+            return jsonify(response)
+        return {"message": "User does not exist in database!"}, 400
