8c32870d9b542a0a7c6ba1a06e488210de33193e
root <root@DESKTOP-OHN0TFT.>
2024-12-05 14:11:45 -0300

    ci: adding python 3.12

--- config.py
+++ config.py
@@ -2,17 +2,16 @@ import os
 from mongomock import MongoClient
 
 
-class DevConfig():
+class DevConfig:
     MONGODB_SETTINGS = {
-            "db": os.getenv("MONGODB_DB"),
-            "host": os.getenv("MONGODB_HOST"),
-            "username": os.getenv("MONGODB_USER"),
-            "password": os.getenv("MONGODB_PASSWORD")
+        "db": os.getenv("MONGODB_DB"),
+        "host": os.getenv("MONGODB_HOST"),
+        "username": os.getenv("MONGODB_USER"),
+        "password": os.getenv("MONGODB_PASSWORD")
     }
 
 
 class ProdConfig:
-
     MONGODB_USER = os.getenv("MONGODB_USER")
     MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
     MONGODB_HOST = os.getenv("MONGODB_HOST")
@@ -20,18 +19,18 @@ class ProdConfig:
     MONGODB_URL = os.getenv("MONGODB_URL")
 
     MONGODB_SETTINGS = {
-            "host": "mongodb+srv://%s:%s@%s/%s?%s" % (
-                MONGODB_USER,
-                MONGODB_PASSWORD,
-                MONGODB_HOST,
-                MONGODB_DB,
-                MONGODB_URL
+        "host": "mongodb+srv://%s:%s@%s/%s?%s" % (
+            MONGODB_USER,
+            MONGODB_PASSWORD,
+            MONGODB_HOST,
+            MONGODB_DB,
+            MONGODB_URL
             )
     }
 
 
 class MockConfig:
     MONGODB_SETTINGS = {
-            "db": "users",
-            "mongo_client_class": MongoClient
+        "db": "users",
+        "mongo_client_class": MongoClient
     }
